a355acfca3e121f1a056e7aa8dd9b5ff75c389de
Heechan <ckstmznf@naver.com>
2022-07-08 15:24:06 +0900

    Initial commit

---
+++ app/src/main/java/kr/edcan/ssf2022/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.edcan.ssf2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
