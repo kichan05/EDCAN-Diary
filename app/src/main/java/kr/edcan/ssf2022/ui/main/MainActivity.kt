a4c663380135590a5d106ad0efedc4c223c9ba63
Heechan <ckstmznf@naver.com>
2022-07-09 14:07:36 +0900

    Register1 layout

--- app/src/main/java/kr/edcan/ssf2022/ui/main/MainActivity.kt
+++ app/src/main/java/kr/edcan/ssf2022/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package kr.edcan.ssf2022
+package kr.edcan.ssf2022.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.edcan.ssf2022.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
