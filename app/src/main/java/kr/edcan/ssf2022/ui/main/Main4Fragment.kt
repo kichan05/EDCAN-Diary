b85ebeaad579dbd98d5f1c5f216fffad39fe9d80
Heechan <ckstmznf@naver.com>
2022-08-11 00:34:56 +0900

    Logout 기능 추가

--- app/src/main/java/kr/edcan/ssf2022/ui/main/Main4Fragment.kt
+++ app/src/main/java/kr/edcan/ssf2022/ui/main/Main4Fragment.kt
@@ -1,18 +1,36 @@
 package kr.edcan.ssf2022.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
 import kr.edcan.ssf2022.R
+import kr.edcan.ssf2022.databinding.FragmentMain4Binding
+import kr.edcan.ssf2022.ui.login.LoginActivity
 
 class Main4Fragment : Fragment() {
+    lateinit var binding : FragmentMain4Binding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main4, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main4, container, false)
+
+        binding.btnMain4Logout.setOnClickListener {
+            val auth : FirebaseAuth = FirebaseAuth.getInstance()
+            auth.signOut()
+
+            val intent = Intent(binding.root.context, LoginActivity::class.java)
+            startActivity(intent)
+            activity!!.finish()
+        }
+
+        return binding.root
     }
 }
