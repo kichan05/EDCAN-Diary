40b633ad5e4f217ae6a6d365605b57c50c76a036
Heechan <ckstmznf@naver.com>
2022-08-20 22:56:15 +0900

    회원가입 다이어 로그

--- app/src/main/java/kr/edcan/ssf2022/ui/register/Register3Fragment.kt
+++ app/src/main/java/kr/edcan/ssf2022/ui/register/Register3Fragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
@@ -13,6 +14,7 @@ import gun0912.tedimagepicker.builder.TedImagePicker
 import kr.edcan.ssf2022.R
 import kr.edcan.ssf2022.databinding.FragmentRegister3Binding
 import kr.edcan.ssf2022.ui.main.MainActivity
+import kr.edcan.ssf2022.util.State
 
 class Register3Fragment : Fragment() {
     lateinit var binding: FragmentRegister3Binding
@@ -30,6 +32,10 @@ class Register3Fragment : Fragment() {
 
             btnRegister3Next.setOnClickListener {
                 (activity as RegisterActivity).run {
+//                    if(viewModel.state.value == State.LOADING){
+//                        Toast.makeText(context, "회원가입 진행중 입니다.", Toast.LENGTH_SHORT).show()
+//                        return@setOnClickListener
+//                    }
                     inputMessage = this@Register3Fragment.viewModel.message.value!!
 
                     register()
--- app/src/main/java/kr/edcan/ssf2022/ui/register/RegisterActivity.kt
+++ app/src/main/java/kr/edcan/ssf2022/ui/register/RegisterActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.google.android.material.snackbar.Snackbar
@@ -22,6 +23,8 @@ class RegisterActivity : AppCompatActivity() {
     lateinit var navController: NavController
     val viewModel : RegisterViewModel by viewModels()
 
+    lateinit var loadingDialog: AlertDialog
+
     // 사용자가 입력한 값들이 들어오는 변수
     var inputName = ""
     var inputEmail = ""
@@ -35,17 +38,25 @@ class RegisterActivity : AppCompatActivity() {
 
         navController = findNavController(R.id.fr_register)
 
+        val dialogView = layoutInflater.inflate(R.layout.dialog_loading, null)
+        loadingDialog = AlertDialog.Builder(this).setView(dialogView).create().apply {
+            setCancelable(false)
+        }
+
         viewModel.state.observe(this){
             when(it){
                 State.SUCCESS -> {
                     Toast.makeText(this, "회원가입에 성공 했습니다.", Toast.LENGTH_SHORT).show()
+                    loadingDialog.dismiss()
                     finish()
                 }
                 State.LOADING -> {
-                    Toast.makeText(this, "회원가입에 중", Toast.LENGTH_SHORT).show()
+//                    Toast.makeText(this, "회원가입에 중", Toast.LENGTH_SHORT).show()
+                    loadingDialog.show()
                 }
                 State.FAIL -> {
-                    Toast.makeText(this, "회원가입에 실패", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    loadingDialog.dismiss()
                 }
             }
         }
@@ -54,7 +65,6 @@ class RegisterActivity : AppCompatActivity() {
     fun register(){
         /*
         * 사용자가 Reguster3에서 회원가입 완료 버튼을 눌렀을때 실행되는 함수
-        *
         * */
         viewModel.userData.value = User(
             name = inputName,
