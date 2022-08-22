93c88e37d0716df0e3d32b327da67dca3b0910b9
Heechan <ckstmznf@naver.com>
2022-08-23 00:40:21 +0900

    DeveloperDonation url

--- app/src/main/java/kr/edcan/ssf2022/util/Url.kt
+++ app/src/main/java/kr/edcan/ssf2022/util/Url.kt
@@ -4,5 +4,5 @@ object Url {
     const val openSource = "https://github.com/ckstmznf/SSF2022-EDCAN"
     const val edcanWeb = "https://www.edcan.kr"
     const val developerGithub = "https://www.github.com/ckstmznf"
-    const val developerDonation = "https://www.toss.me/바키찬희찬"
+    const val developerDonation = "https://toss.me/바키찬희찬"
 }
